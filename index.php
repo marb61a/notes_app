f601a2d2e97f8cf8635d4d7ebeedea82ab54db0b
martin byrne <marb61a@gmail.com>
2016-11-30 14:16:03 +0000

    added initial file and folder structure

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+session_start();
+include('connection.php');
+
+//Logout
+include('logout.php');
+
+// Remember me
+include('remember.php');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Online Notes App</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="styling.css" rel="stylesheet">
+        <link href='https://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        
+    </body>
+</html>
