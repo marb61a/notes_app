e21ebc8016ef1fa60a94566bd3840085f60bab8c
martin byrne <marb61a@gmail.com>
2016-12-01 11:34:21 +0000

    added register form

--- index.php
+++ index.php
@@ -103,6 +103,51 @@ include('remember.php');
                 </div>
             </div>
         </form>
+        
+        
+        <!--Sign up form-->
+        <form method="post" id="signupform">
+            <div class="modal" id="signupModal" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button class="close" data-dismiss="modal">
+                                &times;
+                            </button>
+                            <h4 id="myModalLabel">
+                                Sign Up Today and start using our app!
+                            </h4>
+                        </div>
+                        <div class="modal-body">
+                            <!--Sign up message from PHP file-->
+                            <div id="signupmessage"></div>
+                            <div class="form-group">
+                                <label for="username" class="sr-only">Username</label> 
+                                <input class="form-control" type="text" name="username" id="username" placeholder="Username" maxlength="30">
+                            </div>
+                            <div class="form-group">
+                                <label for="email" class="sr-only">Email</label>
+                                <input class="form-control" type="email" name="email" id="email" placeholder="Email Address" maxlength="50">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="sr-only">Choose a password:</label>
+                                <input class="form-control" type="password" name="password" id="password" placeholder="Choose a password" maxlength="30">    
+                            </div>
+                            <div class="form-group">
+                                <label for="password2" class="sr-only">Confirm password</label>
+                                <input class="form-control" type="password" name="password2" id="password2" placeholder="Confirm password" maxlength="30">   
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <input class="btn green" name="signup" type="submit" value="Sign up">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
        
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
