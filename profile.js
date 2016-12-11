150e47ec55662ec97069e47c3642659c8791a7a3
martin byrne <marb61a@gmail.com>
2016-12-11 19:49:52 +0000

    added updateusername.php

--- profile.js
+++ profile.js
@@ -20,4 +20,26 @@ $("updateusernameform").submit(function(event){
             $("#updateusernamemessage").html("<div class='alert alert-danger'>There was an error with the Ajax Call. Please try again later.</div>");    
         } 
     });
+});
+
+
+// Ajax call to updatepassword.php
+$("updatepasswordform").submit(function(event){
+    // Prevent default processing
+    event.preventDefault();
+    // Collect user inputs
+    var datatopost = $(this).serializeArray();
+    // Send collected inputs to updateusername.php using AJAX
+    $.ajax({
+        url: "updatepassword.php",
+        type: "POST",
+        data: datatopost,
+        success: function(data){
+            if(data){
+                $("#updatepasswordmessage").html(data);
+            }
+        },
+        error: function(){
+            $("#updatepasswordmessage").html("<div class='alert alert-danger'>There was an error with the Ajax Call. Please try again later.</div>");        } 
+    });
 })
