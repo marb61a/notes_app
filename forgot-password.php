34a16ceb6b0c46fa367b82e591a5091742e6f460
martin byrne <marb61a@gmail.com>
2017-01-03 14:11:36 +0000

    updated forgot password

--- forgot-password.php
+++ forgot-password.php
@@ -22,5 +22,27 @@
         exit;
     }
     
+    // If there are no errors
+    // Assign query to variable
+    $email = mysqli_real_escape_string($link, $email);
+   
+    // Run a db query to check if the email exists in the users table
+    $sql = "SELECT * FROM users WHERE email = '$email'";
+    $result = mysqli_query($link, $sql);
+    if(!$result){
+        echo '<div class="alert alert-danger">Error running the query!</div>'; 
+        exit;
+    }
+    $count = mysqli_num_rows($result);
     
+    // Print error message if email deoes not exist
+    if($count != 1){
+        echo '<div class="alert alert-danger">That email does not exist on our database!</div>';  
+        exit;
+    }
+   
+    // Get the user_id
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $user_id = $row['user_id'];
+   
 ?>
