2ddc8fdf1421bfc295a303b32051cbca924ad8fb
martin byrne <marb61a@gmail.com>
2017-01-09 14:06:34 +0000

    update resetpassword file

--- resetpassword.php
+++ resetpassword.php
@@ -26,6 +26,55 @@
         </style>
     </head>
     <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-offset-1 col-sm-10 contactForm">
+                    <h1>Reset Password</h1>
+                    <div id="resultmessage"></div>
+                    <?php
+                        // If user_id or key is missing
+                        if(!isset($_POST['user_id']) || !isset($_POST['key'])){
+                            echo '<div class="alert alert-danger">There was an error. Please click on the link you received by email.</div>';
+                            exit;
+                        }
+                        
+                        // Else, store user_id or key in variables
+                        $user_id = $_GET['user_id'];
+                        $key = $_GET['key'];
+                        $time = time() - 86400;
+                        
+                        // Prepare variables for the db query
+                        $user_id = mysqli_real_escape_string($link, $user_id);
+                        $key = mysqli_real_escape_string($link, $key);
+                        
+                        // Run db query which checks combination of user_id & key exists and is less than 24h old
+                        $sql = "SELECT user_id FROM forgotpassword WHERE rkey='$key' AND user_id='$user_id' AND time > '$time' AND status='pending'";
+                        $result = mysqli_query($link, $sql);
+                            
+                        if(!$result){
+                            echo '<div class="alert alert-danger">Error running the query</div>';
+                            exit;
+                        }
+                        
+                        // If the combination does not exist, show an error
+                        $count = mysqli_num_rows($result);
+                        if($count !== 1){
+                            echo '<div class="alert alert-danger">Please try again.</div>';  
+                            exit;
+                        }
+                        
+                        // Print reset password form with hidden user_id and key fields
+                        echo "
+                            <form method=post id='passwordreset'>
+                                
+                            </form>
+                        ";
+                    ?>
+                </div>
+            </div>
+        </div>
         
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
