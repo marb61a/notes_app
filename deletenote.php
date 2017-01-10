adaa47ca2e6df92038e2e3e70b83b08cc543ce0b
martin byrne <marb61a@gmail.com>
2017-01-10 11:06:49 +0000

    update forgot password file

--- deletenote.php
+++ deletenote.php
@@ -5,4 +5,10 @@
     // Get note ID using AJAX
     $note_id = $_POST['id'];
     
+    // Run a db query to delete note
+    $sql = "DELETE FROM notes WHERE id = $note_id";
+    $result = mysqli_query($link, $sql);
+    if(!$result){
+        echo 'error';   
+    }
 ?>
--- forgot-password.php
+++ forgot-password.php
@@ -44,5 +44,25 @@
     // Get the user_id
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
     $user_id = $row['user_id'];
+    
+    // Create a unique activation code
+    $key = bin2hex(openssl_random_pseudo_bytes(16));
+    
+    // Insert user details and activation code in the forgotpassword table
+    $time = time();
+    $status = 'pending';
+    $sql = "INSERT INTO forgotpassword (`user_id`, `rkey`, `time`, `status`) VALUES ('$user_id', '$key', '$time', '$status')";
+    $result = mysqli_query($link, $sql);
+    if(!$result){
+        echo '<div class="alert alert-danger">There was an error inserting the users details in the database!</div>'; 
+        exit;
+    }
+    
+    // Send email with link to resetpassword.php with user id and activation code
+    $message = "Please click on this link to reset your password:\n\n";
+    $message .= "";
+    if(mail($email, 'Reset your password', $message, 'From:'.'developmentisland@gmail.com')){
+        echo "<div class='alert alert-success'>An email has been sent to $email. Please click on the link to reset your password.</div>";
+    }
    
 ?>
